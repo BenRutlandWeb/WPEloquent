e1455012447fc6014c8117faab5e4b9fcfe2beab
Ben Rutland <benrutland@hotmail.com>
2020-09-16 17:45:19 +0100

    documented taxonomy model

--- src/Models/Taxonomy.php
+++ src/Models/Taxonomy.php
@@ -3,38 +3,71 @@
 namespace WPEloquent\Models;
 
 use WPEloquent\Models\Term;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class Taxonomy extends Model
 {
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
     public $timestamps = false;
 
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = 'term_taxonomy';
 
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
     protected $primaryKey = 'term_taxonomy_id';
 
-    // aliases
-
+    /**
+     * Get the Taxonomy name.
+     *
+     * @return string
+     */
     public function getNameAttribute()
     {
         return $this->taxonomy;
     }
 
-    // scopes
-
-    public function scopeCategory($query)
+    /**
+     * Scope the Taxonomy to categories.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeCategory(Builder $query): Builder
     {
         return $query->where('taxonomy', 'category');
     }
 
-    public function scopeTag($query)
+    /**
+     * Scope the Taxonomy to tags.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeTag(Builder $query): Builder
     {
         return $query->where('taxonomy', 'post_tag');
     }
 
-    // relationships
-
-    public function terms()
+    /**
+     * Get the Taxonomy terms.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\Relation
+     */
+    public function terms(): Relation
     {
         return $this->hasMany(Term::class, 'term_id', 'term_id');
     }
--- src/Scopes/PostTypeScope.php
+++ src/Scopes/PostTypeScope.php
@@ -5,6 +5,7 @@ namespace WPEloquent\Scopes;
 use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Arr;
 
 class PostTypeScope implements Scope
 {
@@ -17,9 +18,6 @@ class PostTypeScope implements Scope
      */
     public function apply(Builder $builder, Model $model): void
     {
-        $builder->whereIn(
-            'post_type',
-            !is_array($model->postType) ? [$model->postType] : $model->postType
-        );
+        $builder->whereIn('post_type', Arr::wrap($model->postType));
     }
 }
